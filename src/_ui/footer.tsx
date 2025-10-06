9d5989d1405e40f21444a031f454ba9a4e81b560
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-06 16:02:18 -0400

    Updated the footer (no links yet)

--- src/_ui/footer.tsx
+++ src/_ui/footer.tsx
@@ -1,12 +1,16 @@
+import Github from "@/_assets/github.svg";
+import YouTube from "@/_assets/youtube.svg";
+
 export function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-      <aside>
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by ACME
-          Industries Ltd
-        </p>
+    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 pr-6">
+      <aside className="w-full">
+        <p>HydraNet</p>
       </aside>
+      <nav className="w-full flex flex-row justify-end gap-4">
+        <img src={Github} alt="GitHub" className="h-6 w-6 invert" />
+        <img src={YouTube} alt="YouTube" className="h-6 w-6 invert" />
+      </nav>
     </footer>
   );
 }
